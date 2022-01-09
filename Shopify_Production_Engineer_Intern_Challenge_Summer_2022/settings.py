b36b9dd3916a3ef4fd324d1129ac1685d724b3c3
Rush <ruspatel1996@gmail.com>
2022-01-09 11:49:14 -0700

    Remove a hardcoded Django secret key

--- Shopify_Production_Engineer_Intern_Challenge_Summer_2022/settings.py
+++ Shopify_Production_Engineer_Intern_Challenge_Summer_2022/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wh14646d%(0&x0$@*6@r6n_gf@&0c*kgm)6j&%_x*=z$6=m+qx'
+# SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
